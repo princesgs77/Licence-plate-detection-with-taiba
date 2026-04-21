717deac15bbaf0c3a7e676428bb8711bb25ff056
Taiba Akhtar <120650461+taibaakhtar@users.noreply.github.com>
2026-04-21 20:35:48 +0530

    Delete net.py

--- net.py
+++
@@ -1,9 +0,0 @@
-from pyngrok import ngrok
-
-ngrok.set_auth_token("3BNuLTSHvX3WcMMpMSG7OhEmmUV_DuiJg9wiPuQ2sigUHJ4K")
-
-public_url = ngrok.connect(4747)
-print("NGROK URL:", public_url)
-
-# 🔥 KEEP PROCESS ALIVE
-input("Press Enter to stop ngrok...")
